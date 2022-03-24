287df158a34a0c0b7be8437750ceca91d973610b
Martin J Frigaard <mjfrigaard@gmail.com>
2022-03-24 09:54:52 -0700

    new workouts

---
+++ _stash.R
@@ -0,0 +1,15 @@
+library(lubridate)
+
+generate_md_dates <- function(start_from, end_with) {
+  
+  strt <- lubridate::ymd(start_from)
+  end <- lubridate::ymd(end_with)
+  
+  dates <- seq(strt, end, by = "1 day")
+  
+  sorted_dates <- sort(dates, decreasing = TRUE)
+  
+  return(cat(paste0("## ", sorted_dates, "\n")))
+  
+}
+
